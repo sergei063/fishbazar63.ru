44f61c46f334895ef3da708aa25148cd8a3c620c
sergei063 <31759077+sergei063@users.noreply.github.com>
2018-10-30 23:03:11 +0400

    db

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {ConnectedRouter, routerMiddleware,} from 'react-router-redux';
-import {Provider} from 'react-redux';
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux';
+import { Provider } from 'react-redux';
 import $ from 'jquery';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import createHistory from 'history/createBrowserHistory';
-import {applyMiddleware, createStore} from 'redux';
-import {css} from 'aphrodite/no-important';
+import { applyMiddleware, createStore } from 'redux';
+import { css } from 'aphrodite/no-important';
 import App from './App';
 import './css/GlobalStyles'; // global styles through Aphrodite
 import allReducers from './reducers';
@@ -58,10 +58,10 @@ var isMobile = {
     any() {
         return (
             isMobile.Android()
-      || isMobile.BlackBerry()
-      || isMobile.iOS()
-      || isMobile.Opera()
-      || isMobile.Windows()
+            || isMobile.BlackBerry()
+            || isMobile.iOS()
+            || isMobile.Opera()
+            || isMobile.Windows()
         );
     },
 };
@@ -69,7 +69,7 @@ var isMobile = {
 ReactDOM.render(
     <Provider store={store}>
         <ConnectedRouter history={history} basename="/fishbazar63.ru">
-            <App history={history} isMobile={isMobile} />
+            <App history={history} isMobile={isMobile}/>
         </ConnectedRouter>
     </Provider>,
     document.getElementById('root'),
@@ -83,14 +83,14 @@ const listenScrollEvent = () => {
     const elem = $('nav');
 
     if (top < h_hght /* || ($(window).width()<=750) */) {
-    // elem.css('top', '');
+        // elem.css('top', '');
         elem.addClass(css(AppStyle.menu_inline_ul));
         elem.removeClass(css(AppStyle.menu_inline_ul_scroll));
     } else {
         elem.removeClass(css(AppStyle.menu_inline_ul));
         elem.addClass(css(AppStyle.menu_inline_ul_scroll));
-    /* elem.css('top', 0);
-         elem.css('position', 'fixed'); */
+        /* elem.css('top', 0);
+             elem.css('position', 'fixed'); */
     }
 };
 const getRandomArbitrary = (min, max) => Math.floor(Math.random() * (max - min)) + min;
